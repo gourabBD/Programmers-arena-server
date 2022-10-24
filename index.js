74c27ca4eac53d40cefce9a8584f409980525165
gourab <rgourab02@gmail.com>
2022-10-25 01:11:38 +0600

    vercel done

--- index.js
+++ index.js
@@ -11,16 +11,18 @@ const courses=require('./data/courses.json');
 app.get('/', (req, res) => {
   res.send('Enjoy!')
 })
+app.get('/courses/:id', (req, res) => {
+  
+    const id=parseInt(req.params.id);
+    const selectedCourses= courses.find(n=> n.id===id);
+    res.send(selectedCourses);
+  })
 
 
 
 app.get('/courses', (req, res) => {
   res.send(courses)
 })
-
-
-
-
 
 app.listen(port, () => {
   console.log(` running on port: ${port}`)
