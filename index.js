630ad0a40d5d60b176a91ee165507dbfdc45d506
gourab <rgourab02@gmail.com>
2022-10-24 23:04:44 +0600

    first commit me

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+const courses=require('./data/courses.json')
+app.get('/', (req, res) => {
+  res.send('Enjoy!')
+})
+
+
+
+app.get('/courses', (req, res) => {
+  res.send(courses)
+})
+
+app.get('/courses/:id', (req, res) => {
+  
+  const id=parseInt(req.params.id);
+  const selectedCourses= courses.find(n=> n.id===id);
+  res.send(selectedCourses);
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Live life running on port: ${port}`)
+})
